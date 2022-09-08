935250347f2bcd3f832712c1d09d418846a5c8ef
pranavpalakodety <pranavpalakodety@localhost>
2022-09-08 13:54:16 -0500

    studio 1

--- src/studio1/HiFour.java
+++ src/studio1/HiFour.java
@@ -19,6 +19,8 @@ public class HiFour {
 		String s3 = in.nextLine();
 		//
 		// Say hello to the names in s0 through s3.
+		
+		System.out.println("Hello " + s0 + ", " + s1 + ", " + s2 + ", " + s3);
 		//
 
 	}
